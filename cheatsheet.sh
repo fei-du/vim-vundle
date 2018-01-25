23e41e35bb17842ef69c6f55c9a6a21b634294f5
Fei Du <fei.du@nxp.com>
2018-01-25 21:05:32 +0800

    add sed examples and regex

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -
 set -o nounset                                  # Treat unset variables as an error
 
-[find]
+# [find]
     # find location comparison-criteria search-term
     find
     find examples/
@@ -57,7 +57,7 @@ set -o nounset                                  # Treat unset variables as an er
     -delete
     -exec
 
-[grep]
+# [grep]
     # grep "string" file_pattern
     grep this demo_file*
     grep -i line demo_file
@@ -83,18 +83,18 @@ set -o nounset                                  # Treat unset variables as an er
     grep -o is.*line demo_file
     grep IFS * -n -C 3
 
-[tar]
+# [tar]
     # tar function [options] object1 object2
     tar -cvf a.tar gitconfig setup.sh
     tar -tf a.tar
     tar -xvf a.tar
 
-[option]
+# [option]
     getopt ab:cd -a -b test1 -cd test2 test4
     getopt ab:cd -a -b test1 -cde test2 test4
     getopt -q ab:cd -a -b test1 -cde test2 test4
 
-[xargs]
+# [xargs]
     # read data from stdin and executes command(supplied) one or more times based on the input read
     find ./music -name "*.mp3" -print0 | xargs -0 ls
     # find all file in work folder, pass to grep and search for profit
@@ -124,7 +124,7 @@ set -o nounset                                  # Treat unset variables as an er
     # construct a command containing a specific number of arguments and to insert those into the middle of a command line
     ls | xargs -n 6 | xargs -I {} echo {} - some files in the directory
 
-[misc]
+# [misc]
     # without the dollar sign, the shell interprest the variable name as a normal text string
     # math equation $[1+ 5] supports only integer arithmetic
     # exit status codes can only go up to 255
@@ -135,7 +135,12 @@ set -o nounset                                  # Treat unset variables as an er
     # use subshell to capture the output of a function to a shell variable
     # variable defined in function is global, unless use local keyword.
 
-[sed]
+# [git]
+    # check whether remote has changed
+    git ls-remote origin -h refs/heads/master
+
+# [sed]
+    # sed conforms only to a subset of the BRE for the speed
     # search
 	s/pattern/replacement/flags
 	    a number: indicating pattern occurence for which new text shold be subsituted
@@ -191,3 +196,140 @@ set -o nounset                                  # Treat unset variables as an er
 	sed '$r test.txt' data1.txt
 	# a cool application of the read command is to use it in conjunction with a delete
 	# to replace a placeholder in a file with data from another file
+	# h copy pattern space to hold space
+	# H append pattern space to hold space
+	# g copy hold space to pattern space
+	# G append hold space to pattern space
+	# x exchanges
+	# b branching: if label is not present, the b command proceeds to the end
+	# t if the s command succeessfully matches, the t branches to a label, if label is not present, it branches to the end.
+	# & the ampersand symbol is used to represent the matching pattern in s command
+	# grouping substring: using parentheses in s command, use the escape character to identify them as grouping characters
+	sed '/header/{n ; d}' data1.txt
+	sed '/header/{n ; d}' data1.txt  | tail -n 4
+	sed '/first/{N ; s/\n/ /}' data2.txt
+	sed 'N ; s/system Administrator/Desktop User/' data2.txt
+	sed 'N ; s/system.Administrator/Desktop User/' data2.txt
+	sed 'N; /system\nAdm/d' data2.txt
+	sed 'N; /system\nAdm/D' data2.txt
+	sed -n 'N ; /system\nAdmi/P' data2.txt
+	sed -n '/first/ { h; p; n; p; g; p}' data2.txt
+	sed -n '{1!G; h; $p}' data2.txt
+	sed '{2,3b ; s/this is/Is this/ ; s/line/test?/}' data2.txt
+	sed '{/first/b jump1; s/this is the/No jump on/
+	    :jump1
+	    s/this is the/Jump here on/}' data2.txt
+	echo "this, is, a, test, to, remove, commas." | sed -n '{:start
+	    s/,//1p
+	    /,/b start
+	}'
+	sed '{
+	    s/first/matched/
+	    t
+	    s/this is the/No match on/
+	}' data2.txt
+	echo "this, is, a, test, to, remove, commas." | sed -n '{:start
+	    s/,//1p
+	    t start
+	}'
+	echo "ths cat sleeps in his hat." | sed 's/.at/"&"/g'
+	echo "that furry cat is pretty" | sed 's/furry \(.at\)/\1/'
+	echo "that furry hat is pretty" | sed 's/furry \(.at\)/\1/'
+	# using substring components to insert a comma in long numbers
+	echo "45657612345asd1234567" | sed '{
+	    :start
+	    s/\(.*[0-9]\)\([0-9]\{3\}\)/\1,\2/
+	    t start
+	}'
+    # [sed utilities]
+	# spacing with double lines
+	sed "G" data1.txt
+	# insert two blank line
+	sed 'G;G' data1.txt
+	sed "$!G" data1.txt
+	# spacing file with blanks, first delete existing blank lines
+	sed '/^$/d; $!G' data1.txt
+	# numbering lines in a file
+	sed '=' data1.txt | sed 'N; s/\n/ /'
+	# deleting consecutive blank lines
+	# the key is to create an address range including a non-blank line and a blank line. if sed comes across this range, it shouldn't delete the line.
+	sed '/./,/^$/!d' data1.txt
+	# deleting leading blank lines
+	sed '/./,$!d' data1.txt
+	# deleting trailing blank lines
+	# braces within normal script braces allow you to group commands together. the address pattern matches any line that contains only newline character. when found, if the last line, the delete command delete it. if not, the N append next line to it, and branch to the beginning.
+	sed '{
+	    :start
+	    /^\n*$/{$d; N; b start}
+	}' data1.txt
+	# view a range of lines of a file
+	sed -n '5,10p' setup.sh
+	# view entire file except a given range
+	sed '20,30d' setup.sh
+	# view non-consecutive lines
+	sed -n -e '5,7p' -e '10,13p' setup.sh
+	# ignore character case
+	sed 's/version/story/gi' setup.sh
+	ip route show | sed 's/ */ /g'
+	# in-place editing and backing up original file
+	sed -i'.orig' 's/this/that/gi' data1.txt
+	# switching pairs of words
+	echo "white,Walter"  | sed 's/^\(.*\),\(.*\)$/\2, \1/g'
+	# display starting with "Of course", ending with "attention"
+	sed -n '/Of course/,/attention/p' myfile
+	# write all commands in .sed and execute them
+	sed -f script.sed file.txt
+	# replace ham with cheese except 5th line
+	sed '5!s/ham/cheese/' file.txt
+	# delete last line
+	sed '$d' file.txt
+	# print lines with three consecutive digits
+	sed '/[0-9]\{3\}/p' file.txt
+	# unless boom found, replace  aaa with bb
+	sed '/boom/!s/aaa/bb/' file.txt
+	# delete all lines from line 17 to 'disk'
+	sed '17,/disk/d' file.txt
+	# replace foo with bar for the 4th instance
+	sed 's/foo/bar/4'
+	# delete every third line start from the first
+	sed '1~3d' data1.txt
+
+# [RegEX]
+    # [general]
+	    echo "this is a test" | sed -n '/test/p'
+	    echo "this is a test" | sed -n '/trial/p'
+	    echo "this is a test" | gawk '/test/{print $0}'
+	    echo "\ is a special character" | sed -n '/\\/p'
+	    echo "3 / 2" | sed -n '/\//p'
+	    echo "this ^ is a test" | sed -n '/s ^/p'
+    # [character classes]
+	    echo "yes" | sed -n '/[Yy]es/p'
+    # [special character classes]
+	# [[:alpha:]] [[:alnum:]] [[:blank:]] [[:digit:]] [[:lower:]] [[:print:]] [[:space:]] [[:upper:]]
+	    echo "abc124" | sed -n '/[[:alpha:]]/p'
+	    echo "this is, a test" | sed -n '/[[:punct:]]/p'
+	    echo "baeeaaet" | sed -n '/b[ae]*t/p'
+    # ERE you can use in gawk
+	# question mark: zero or one time
+	    echo "bt" | gawk '/be?t/{print $0}'
+	    echo "bet" | gawk '/be?t/{print $0}'
+	    echo "beet" | gawk '/be?t/{print $0}'
+	    echo "bt" | gawk '/b[ae]?t/{print $0}'
+	    echo "bet" | gawk '/b[ae]?t/{print $0}'
+	    echo "bat" | gawk '/b[ae]?t/{print $0}'
+	    echo "beat" | gawk '/b[ae]?t/{print $0}'
+	# plus sign: one or more times
+	    echo "bat" | gawk '/b[ae]+t/{print $0}'
+	    echo "baet" | gawk '/b[ae]+t/{print $0}'
+	    echo "baeaeat" | gawk '/b[ae]+t/{print $0}'
+	# braces: a limit on a repeatable regular expression
+	    echo "bt" | gawk --re-interval '/be{1}t/{print $0}'
+	    echo "bet" | gawk --re-interval '/be{1}t/{print $0}'
+	    echo "beet" | gawk --re-interval '/be{1}t/{print $0}'
+	# pipe symbol: in a logical OR formula
+	    echo "The cat is asleep" | gawk '/cat|dog/{print $0}'
+	    echo "The dog is asleep" | gawk '/cat|dog/{print $0}'
+	# grouping expressions
+	echo "Sat" | gawk '/Sat(urday)?/{print $0}'
+	echo "Saturday" | gawk '/Sat(urday)?/{print $0}'
+	echo $PATH | sed 's/:/ /g'
